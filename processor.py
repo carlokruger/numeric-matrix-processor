79f378ffa761f81764863af5f374e3ccff2ef0ac
carlokruger <oUtME7tNxmfRnLxwgTxvUiUJ>
2020-09-05 13:32:05 +1200

    completed Stage 2

--- processor.py
+++ processor.py
@@ -1,30 +1,20 @@
 X = []
-Y = []
-
-
 a = []
-b = []
+
 
 a = [int(i) for i in input().split()]
+
 for _ in range(a[0]):
     X.append([int(val) for val in input().split()])
 
-b = [int(j) for j in input().split()]
-for _ in range(b[0]):
-    Y.append([int(x) for x in input().split()])
+multiplier = int(input())
 
-#line = [0] * a[1]
-#for _ in range(a[0]):
-#    result.append(line)
+
 result = X
-#print(result)
 
-if a[0] != b[0] or a[1] != b[1]:
-    print("ERROR")
-else:
-    for i in range(len(result)):
-        # iterate through columns
-        for j in range(len(result[0])):
-            result[i][j] += Y[i][j]
-    for item in result:
-        print(' '.join(str(x) for x in item))
+for i in range(len(result)):
+    # iterate through columns
+    for j in range(len(result[0])):
+        result[i][j] *= multiplier
+for item in result:
+    print(' '.join(str(x) for x in item))
